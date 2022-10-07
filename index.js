1a1ede2ee1fbb13accef10c99b1160680a24cd4e
charmig03 <108714080+charmig03@users.noreply.github.com>
2022-10-08 00:43:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  http
+    .createServer((request, response) => {
+      response.writeHeader(200, { "Content-Type": "text/html" });
+      response.write(home);
+      response.end();
+    })
+    .listen(3000);
+});
